2bdf042aa50c373a3d4d1fa882a6310474c976c8
Steven Only <SeiryuZ@hotmail.com>
2012-07-19 14:19:34 +0700

    initial klikevent-RoR commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klikevent::Application.config.secret_token = '39cf1a463872f98d258510b07a2f7808eea149a4e7bc44f38064aeb9bf668910b67a9e17fd39c35b7df13a40407efdd9a2a53d95562269eee6adf7dfcc40a1dd'
