3da1cf3eaf570d5c410165ee5159514e7a0d16c3
Steven Only <SeiryuZ@hotmail.com>
2012-07-21 21:51:27 +0700

    precompiled assets

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,10 +8,7 @@ class Event < ActiveRecord::Base
   has_many :event_event_times
 
   has_attached_file :cover_image,
-    :storage => :s3,
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :s3_credentials => {
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    :storage => :s3, 
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :path => "events/:id/cover/:id.:extension"
 end
