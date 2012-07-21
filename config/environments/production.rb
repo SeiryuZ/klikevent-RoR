4a6eb8231f05dc2f90460328275a1d19ab767b41
Steven Only <SeiryuZ@hotmail.com>
2012-07-21 21:57:32 +0700

    changed some config for production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,13 @@ Klikevent::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.serve_static_assets = true
+
 
   #devise setup
   config.action_mailer.default_url_options = { :host => 'www.klikevent.com' }
